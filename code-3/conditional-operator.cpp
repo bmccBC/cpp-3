6def1717e680a4f8652c5a0abe5c5b5da3e6dc9a
bmccBC <57549670+bmccBC@users.noreply.github.com>
2019-11-19 22:05:21 -0500

    Add files via upload

---
+++ code-3/conditional-operator.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+  // local variable declaration:
+  int a = 10;
+
+  a < 20 ? cout << "a is less than 20" << endl : cout << "value of a is : " << a << endl;
+
+  return 0;
+}
